db3687ebd2495e9510893918333fac5de6508037
O. David Edekovwere <davidedek2000@gmail.com>
2022-06-24 13:52:46 +0100

    Create intro2.js

---
+++ intro2.js
@@ -0,0 +1,8 @@
+alert("My name is Oghenefejiro David Edekovwere. My height is 6.0 feets and I am from Nigeria.")
+var name = ('I am Oghenefejiro David Edekovwere');
+var height = ('6.0 ft');
+var country = ('Nigeria');
+
+console.log(name);
+console.log(height);
+console.log(country);
